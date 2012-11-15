4273bdba059b495834c904ec785c9ad3cc0da102
AndroidRul3z <ferrettidario@gmail.com>
2012-11-15 14:31:48 +0100

    Fix device asserts
    Conflicts:
    	tools/releasetools/edify_generator.py

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -92,10 +92,10 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    cmd = ('getprop("ro.product.device") == "%s" || '
-           'abort("This package is for \\"%s\\" devices; '
-           'this is a \\"" + getprop("ro.product.device") + "\\".");'
-           ) % (device, device)
+    cmd = ('assert(' +
+           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
+                         % (i, i) for i in device.split(",")]) +
+           ');')
     self.script.append(cmd)
 
   def AssertSomeBootloader(self, *bootloaders):
@@ -282,3 +282,4 @@ class EdifyGenerator(object):
       data = open(os.path.join(input_path, "updater")).read()
     common.ZipWriteStr(output_zip, "META-INF/com/google/android/update-binary",
                        data, perms=0755)
+
